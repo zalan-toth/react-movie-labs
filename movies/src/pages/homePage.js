a6c4df248e153a03cf13e03eb2dc2d769463df28
Zalán Tóth <zalant.tsc@gmail.com>
2024-10-11 15:54:35 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -34,6 +34,12 @@ const HomePage = (props) => {
             return genreId > 0 ? m.genre_ids.includes(genreId) : true;
         });
 
+    const addToFavorites = (movieId) => {
+        const updatedMovies = movies.map((m) =>
+            m.id === movieId ? { ...m, favorite: true } : m
+        );
+        setMovies(updatedMovies);
+    };
     const handleChange = (type, value) => {
         if (type === "name") setNameFilter(value);
         else setGenreFilter(value);
@@ -51,7 +57,7 @@ const HomePage = (props) => {
                         genreFilter={genreFilter}
                     />
                 </Grid>
-                <MovieList movies={displayedMovies} />
+                <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
             </Grid>
         </Grid>
     );
