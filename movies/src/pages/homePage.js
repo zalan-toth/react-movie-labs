a01a3e5658e532c15ac995bb010b2b8779cfd062
Zalán Tóth <zalant.tsc@gmail.com>
2024-10-11 16:05:42 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,38 +1,11 @@
 import React, { useState, useEffect } from "react";
-import MovieList from "../components/movieList";
-import Grid from "@mui/material/Grid2";
-import Header from '../components/headerMovieList';
-import FilterCard from "../components/filterMoviesCard";
+import PageTemplate from '../components/templateMovieListPage'
+import { getMovies } from "../api/tmdb-api";
 
 const HomePage = (props) => {
     const [movies, setMovies] = useState([]);
-
-    const [nameFilter, setNameFilter] = useState("");
-    const [genreFilter, setGenreFilter] = useState("0");
-
-    useEffect(() => {
-        fetch(
-            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-        )
-            .then((res) => res.json())
-            .then((json) => {
-                console.log(json);
-                return json.results;
-            })
-            .then((movies) => {
-                setMovies(movies);
-            });
-    }, []);
-
-    const genreId = Number(genreFilter);
-
-    let displayedMovies = movies
-        .filter((m) => {
-            return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
-        })
-        .filter((m) => {
-            return genreId > 0 ? m.genre_ids.includes(genreId) : true;
-        });
+    const favorites = movies.filter(m => m.favorite)
+    localStorage.setItem('favorites', JSON.stringify(favorites))
 
     const addToFavorites = (movieId) => {
         const updatedMovies = movies.map((m) =>
@@ -40,26 +13,19 @@ const HomePage = (props) => {
         );
         setMovies(updatedMovies);
     };
-    const handleChange = (type, value) => {
-        if (type === "name") setNameFilter(value);
-        else setGenreFilter(value);
-    };
+
+    useEffect(() => {
+        getMovies().then(movies => {
+            setMovies(movies);
+        });
+    }, []);
+
     return (
-        <Grid container>
-            <Grid size={12}>
-                <Header title={"Home Page"} />
-            </Grid>
-            <Grid container>
-                <Grid key="find" size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-                    <FilterCard
-                        onUserInput={handleChange}
-                        titleFilter={nameFilter}
-                        genreFilter={genreFilter}
-                    />
-                </Grid>
-                <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
-            </Grid>
-        </Grid>
+        <PageTemplate
+            title='Discover Movies'
+            movies={movies}
+            selectFavorite={addToFavorites}
+        />
     );
 };
 export default HomePage;
