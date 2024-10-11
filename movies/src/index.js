8770e04c192e8b3a17ba683b275558bfec813a6a
Zalán Tóth <zalant.tsc@gmail.com>
2024-10-11 15:31:25 +0100

    Movie Details Page rendering.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
+import MovieDetailsPage from './pages/movieDetailsPage'
 
 const sample = {
     adult: false,
@@ -82,12 +83,23 @@ const sample = {
     vote_average: 7,
     vote_count: 9692,
 };
+const images = [
+    "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+    "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+    "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+    "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
+//const App = () => {
+//    return (
+//        <HomePage movies={movies} />
+//    );
+//};
 const App = () => {
     return (
-        <HomePage movies={movies} />
+        <MovieDetailsPage movie={sample} images={images} />
     );
 };
 
